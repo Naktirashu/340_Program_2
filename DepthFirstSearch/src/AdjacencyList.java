a317c2b7511a29b79f41b51d74ca26bb2fe17cfc
Naktirashu <bx4151gt@metrostate.edu>
2016-10-25 21:21:06 -0500

    added comment

--- DepthFirstSearch/src/AdjacencyList.java
+++ DepthFirstSearch/src/AdjacencyList.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class AdjacencyList {
+	
+	ArrayList<LinkedList<Node>> adj_list = new ArrayList<LinkedList<Node>>();
+	
+	//adj_list.add(new LinkedList<Integer>());
+	
+	/*Say now you want to add numbers 3,4,5 to your first linked lists.Do the following:
 
+	adj_list.get(0).add(3);
+	adj_list.get(0).add(4);
+	adj_list.get(0).add(5);*/
+	
 }
